30c122d4f6648cd3fb702e44014f0000c3add136
Sam <ophisbonklol@gmail.com>
2018-06-20 22:33:37 +0100

    added loadtype enum

---
+++ src/main/java/samophis/lavalink/client/entities/LoadType.java
@@ -0,0 +1,36 @@
+/*
+   Copyright 2018 Samuel Pritchard
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package samophis.lavalink.client.entities;
+
+import samophis.lavalink.client.util.Asserter;
+import javax.annotation.Nonnull;
+
+public enum LoadType {
+    TRACK_LOADED,
+    PLAYLIST_LOADED,
+    SEARCH_RESULT,
+    NO_MATCHES,
+    LOAD_FAILED,
+    UNKNOWN;
+    public static LoadType from(@Nonnull String name) {
+        Asserter.requireNotNull(name);
+        for (LoadType type : values())
+            if (type.name().equals(name))
+                return type;
+        return UNKNOWN;
+    }
+}
